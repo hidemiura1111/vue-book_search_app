a3fba4acccaaba9d6742a18511b518bef32158a9
hidems <miura.eleven@gmail.com>
2023-06-10 17:04:01 +0200

    Change v-container location

--- src/pages/BookSearch.vue
+++ src/pages/BookSearch.vue
@@ -1,60 +1,58 @@
 <template>
   <div>
-    <v-container>
-      <v-row>
-        <v-col cols="6">
-          <v-text-field
-            v-model="keyword"
-            label="Search Book Titile"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="3">
-          <v-btn
-            color="primary"
-            @click="search(keyword)"
-          >
-          Search
-          </v-btn>
-        </v-col>
-        <v-col cols="3">
-          <v-btn
-            color="secondary"
-            to="/"
-          >
-            Index
-          </v-btn>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col
-          cols="12"
-          md="6"
-          v-for="(book, index) in searchResults" :key="book.index"
+    <v-row>
+      <v-col cols="6">
+        <v-text-field
+          v-model="keyword"
+          label="Search Book Titile"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="3">
+        <v-btn
+          color="primary"
+          @click="search(keyword)"
         >
-          <v-card class="mx-auto">
-            <v-row>
-              <v-col cols="2">
-                <v-img :src="book.img"/>
-              </v-col>
-              <v-col cols="10">
-                <v-card-title>{{ book.title }}</v-card-title>
-                <v-card-text>{{ book.description }}</v-card-text>
-                <v-spacer></v-spacer>
-                <v-card-actions>
-                  <v-btn fab dark color="indigo"
-                    @click="addBookList(index)"
-                  >
-                    <v-icon>mdi-plus</v-icon>
-                  </v-btn>
-                </v-card-actions>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+        Search
+        </v-btn>
+      </v-col>
+      <v-col cols="3">
+        <v-btn
+          color="secondary"
+          to="/"
+        >
+          Index
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="12"
+        md="6"
+        v-for="(book, index) in searchResults" :key="book.index"
+      >
+        <v-card class="mx-auto">
+          <v-row>
+            <v-col cols="2">
+              <v-img :src="book.img"/>
+            </v-col>
+            <v-col cols="10">
+              <v-card-title>{{ book.title }}</v-card-title>
+              <v-card-text>{{ book.description }}</v-card-text>
+              <v-spacer></v-spacer>
+              <v-card-actions>
+                <v-btn fab dark color="indigo"
+                  @click="addBookList(index)"
+                >
+                  <v-icon>mdi-plus</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
