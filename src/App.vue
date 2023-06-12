de2fea42fc8d21572651b1aa12a92e1652893abd
hidems <miura.eleven@gmail.com>
2023-06-12 21:57:26 +0200

    Mounted, add, remove, save on local storage

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
 <script>
 import HeaderComponent from '@/global/HeaderComponent';
 import FooterComponent from '@/global/FooterComponent';
+const STORAGE_KEY = 'books';
 
 export default {
   name: 'App',
@@ -20,8 +21,39 @@ export default {
     HeaderComponent,
     FooterComponent,
   },
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      books: [],
+      newBook: null,
+    };
+  },
+  mounted() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+      try {
+        this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+      } catch(e) {
+        localStorage.removeItem(STORAGE_KEY);
+      }
+    }
+  },
+  methods: {
+    addBook() {
+      if (!this.newBook) {
+        return;
+      }
+
+      this.books.push(this.newBook);
+      this.newBook = '';
+      this.saveBooks();
+    },
+    removeBook(x) {
+      this.books.splice(x, 1);
+      this.saveBooks();
+    },
+    saveBooks() {
+      const parsed = JSON.stringify(this.books);
+      localStorage.setItem(STORAGE_KEY, parsed);
+    }
+  }
 };
 </script>
