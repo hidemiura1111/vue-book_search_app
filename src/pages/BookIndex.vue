e8d141fb1d16395a6047835cbc0f00af86a40272
hidems <miura.eleven@gmail.com>
2023-07-05 21:28:45 +0200

    Create Page for BookIndex

--- src/pages/BookIndex.vue
+++ src/pages/BookIndex.vue
@@ -1,9 +1,46 @@
 <template>
-  <div>Book Index</div>
+  <div>
+    <v-row>
+      <v-col cols="6">
+        <v-btn color="primary" to="/search">Search</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6" v-for="book in books" :key="book.id">
+        <v-card>
+          <v-row>
+            <v-col cols="4">
+              <v-img :src="book.image"></v-img>
+            </v-col>
+            <v-col cols="8">
+              <v-card-title>{{ book.title }}</v-card-title>
+              Read Date: {{ book.readDate }}
+              Impressions: {{ book.memo }}
+              <v-spacer></v-spacer>
+              <v-card-actions>
+                <v-btn
+                  :to="{name: 'BookEdit', params: {id: book.id} }"
+                  color="indigo" fab small dark
+                >
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
+  props: {
+    books: {
+      type: Array,
+      required: false,
+    },
+  },
 
 }
 </script>
