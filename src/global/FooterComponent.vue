1f9a732ab2f67b011c4b91d8b0ebaaecd5745304
hidems <miura.eleven@gmail.com>
2023-07-15 19:08:56 +0200

    Create Footer component

--- src/global/FooterComponent.vue
+++ src/global/FooterComponent.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>Footer</div>
+  <v-footer
+    color="primary"
+    dark
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-col
+        class="primary py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Book Search APP</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
 </template>
 
 <script>
 export default {
-
+  name: 'FooterComponent',
 }
 </script>
 
